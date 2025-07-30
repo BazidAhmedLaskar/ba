47c6561f6af8396d04521c1709b25578f931e189
BazidAhmedLaskar <145913702+BazidAhmedLaskar@users.noreply.github.com>
2025-07-30 19:52:08 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,15 +1,24 @@
 // firebase-messaging-sw.js
-importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-
-     apiKey: "AIzaSyAvQ5-KLbsiBY7LS2uOGUF0UR0zBke9K6I",
-    authDomain: "merasmm-c89f4.firebaseapp.com",
-    projectId: "merasmm-c89f4",
-    storageBucket: "merasmm-c89f4.firebasestorage.app",
-    messagingSenderId: "721705668276",
-    appId: "1:721705668276:web:e1f5acde72b84409afd1e1",
+  apiKey: "AIzaSyAvQ5-KLbsiBY7LS2uOGUF0UR0zBke9K6I",
+  authDomain: "merasmm-c89f4.firebaseapp.com",
+  projectId: "merasmm-c89f4",
+  storageBucket: "merasmm-c89f4.appspot.com",
+  messagingSenderId: "721705668276",
+  appId: "1:721705668276:web:e1f5acde72b84409afd1e1"
 });
 
 const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
