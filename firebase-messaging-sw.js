b3cd1635641325108bfe7d5b86de08260c019ee6
BazidAhmedLaskar <145913702+BazidAhmedLaskar@users.noreply.github.com>
2025-07-30 20:02:36 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 // firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyAvQ5-KLbsiBY7LS2uOGUF0UR0zBke9K6I",
@@ -8,17 +8,18 @@ firebase.initializeApp({
   projectId: "merasmm-c89f4",
   storageBucket: "merasmm-c89f4.appspot.com",
   messagingSenderId: "721705668276",
-  appId: "1:721705668276:web:e1f5acde72b84409afd1e1"
+  appId: "1:721705668276:web:e1f5acde72b84409afd1e1",
 });
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function(payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-  };
+messaging.onBackgroundMessage((payload) => {
+  console.log('[Service Worker] Push Received:', payload);
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  const { title, body } = payload.notification;
+
+  self.registration.showNotification(title, {
+    body,
+    icon: '/image.png',
+  });
 });
